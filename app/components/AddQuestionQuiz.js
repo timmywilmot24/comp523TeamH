47e357e63f9d2afad559419d045ad582f4308a57
timmywilmot24 <timmywilmot24@users.noreply.github.com>
2021-04-30 03:04:12 -0400

    Styled add question quiz and quiz stuff

--- app/components/AddQuestionQuiz.js
+++ app/components/AddQuestionQuiz.js
@@ -8,12 +8,15 @@ import {
   Dimensions,
   StyleSheet,
   Image,
+  Pressable,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { FlatGrid } from "react-native-super-grid";
 import CheckBox from "./CheckBox";
 const screenWidth = Dimensions.get("window").width;
 import LoadingAnimationScreen from "./LoadingAnimationScreen.js";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import IGRotator from "./IGRotator";
 
 export default class AdminQuiz extends Component {
   constructor(props) {
@@ -210,14 +213,18 @@ export default class AdminQuiz extends Component {
   }
 
   renderSubjects(response) {
-    let subjects = [];
+    let subjects = [<Text key={-1}>Subjects: </Text>];
     for (let i = 0; i < response.subject.length; i++) {
       subjects.push(<Text key={i}>{response.subject[i]}</Text>);
+      if (i < response.subject.length - 1) {
+        subjects.push(<Text key={i + "1"}>, </Text>);
+      }
     }
-    return <View>{subjects}</View>;
+    return <View style={{ flexDirection: "row" }}>{subjects}</View>;
   }
   render() {
-    let checkBoxList = [];
+    let checkBoxList1 = [];
+    let checkBoxList2 = [];
     let subjects = [
       "Business",
       "Law and Politics",
@@ -229,132 +236,245 @@ export default class AdminQuiz extends Component {
       "Medicine",
     ];
     for (let i = 0; i < 8; i++) {
-      checkBoxList.push(
-        <CheckBox
-          key={i}
-          subject={subjects[i]}
-          setState={(check) => {
-            this.handleCheck(subjects[i], check);
-          }}
-        />
-      );
+      if (i < 4) {
+        checkBoxList1.push(
+          <CheckBox
+            key={i}
+            subject={subjects[i]}
+            setState={(check) => {
+              this.handleCheck(subjects[i], check);
+            }}
+          />
+        );
+      } else {
+        checkBoxList2.push(
+          <CheckBox
+            key={i}
+            subject={subjects[i]}
+            setState={(check) => {
+              this.handleCheck(subjects[i], check);
+            }}
+          />
+        );
+      }
     }
     return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        {this.state.uploadLoading ? (
-          <LoadingAnimationScreen></LoadingAnimationScreen>
-        ) : (
-          <View style={{ marginBottom: (screenWidth * 1) / 3.75 }}>
-            <TextInput
-              placeholder="Category"
-              value={this.state.categoryName}
-              onChangeText={(categoryName) => this.setState({ categoryName })}
-              label="CategoryName"
-            />
-            <FlatGrid
-              onEndReachedThreshold={0.5}
-              itemDimension={screenWidth}
-              data={this.state.responses}
-              renderItem={({ item }) => (
-                <View>
-                  <ImageBackground
-                    style={styles.responseImage}
-                    source={{
-                      uri: item.pic,
-                    }}
-                  >
-                    <Text>{item.action}</Text>
-                  </ImageBackground>
-                  <Text>Subjects</Text>
-                  {this.renderSubjects(item)}
-                  <Text onPress={() => this.handleDeleteResponse(item.action)}>
-                    Delete
-                  </Text>
-                </View>
-              )}
-            />
-            {this.state.responseAdd ? (
-              <View>
-                <Text onPress={() => this.addPicture()}>Add Picture</Text>
-                {this.state.newImage !== "" ? (
-                  <View>
-                    <Image
-                      style={{ width: 100, height: 100 }}
-                      source={{ uri: this.state.newImage }}
-                    />
-                  </View>
-                ) : (
-                  <View
-                    style={{ width: 100, height: 100, backgroundColor: "gray" }}
-                  ></View>
-                )}
-                <TextInput
-                  placeholder="Response action here"
-                  value={this.state.newAction}
-                  onChangeText={(newAction) => this.setState({ newAction })}
-                  label="newAction"
-                ></TextInput>
-                <Text>Subjects</Text>
-                {checkBoxList}
-                <Text>{this.state.newResponseError}</Text>
-                <Text onPress={() => this.handleAddResponse()}>
-                  Add response
-                </Text>
-                <Text
-                  onPress={() =>
-                    this.setState({
-                      responseAdd: false,
-                      newImage: "",
-                      newAction: "",
-                      newSubjects: [],
-                      newResponseError: "",
-                    })
+      <View>
+        <View style={addQuestionStyles.backButtonContainer}>
+          <Pressable
+            style={addQuestionStyles.buttonStuff}
+            onPress={() => this.props.setState(false)}
+          >
+            <Ionicons
+              style={addQuestionStyles.backIcon}
+              size={25}
+              name="caret-back"
+            ></Ionicons>
+            <Text style={addQuestionStyles.backText}>Back</Text>
+          </Pressable>
+        </View>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          {this.state.uploadLoading ? (
+            <LoadingAnimationScreen></LoadingAnimationScreen>
+          ) : (
+            <View style={{ marginBottom: (screenWidth * 1) / 3.75 }}>
+              <TextInput
+                placeholder="Type category name here..."
+                value={this.state.categoryName}
+                onChangeText={(categoryName) => this.setState({ categoryName })}
+                style={addQuestionStyles.categoryText}
+                label="CategoryName"
+              />
+              {this.state.responses.length > 0 && (
+                <IGRotator
+                  responses={this.state.responses}
+                  setState={(responses) =>
+                    this.setState({ responses: responses })
                   }
-                >
-                  Cancel
-                </Text>
-              </View>
-            ) : (
-              <Text onPress={() => this.setState({ responseAdd: true })}>
-                Add Response
-              </Text>
-            )}
-            {this.state.responses.length > 1 && (
-              <View>
-                <Text
-                  onPress={() => {
-                    this.handleAddQuestion();
+                ></IGRotator>
+              )}
+              {this.state.responseAdd ? (
+                <View
+                  style={{
+                    backgroundColor: "white",
+                    marginHorizontal: 20,
+                    borderRadius: 10,
+                    marginTop: 20,
+                    marginBottom: 180,
                   }}
                 >
-                  Add new question
-                </Text>
-                <Text>{this.state.newQuestionError}</Text>
-              </View>
-            )}
-            <Text
-              onPress={() => {
-                this.props.setState(false);
-              }}
-            >
-              Cancel
-            </Text>
-          </View>
-        )}
-      </ScrollView>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      margin: 8,
+                      width: screenWidth - 56,
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <Pressable
+                      onPress={() =>
+                        this.setState({
+                          responseAdd: false,
+                          newImage: "",
+                          newAction: "",
+                          newSubjects: [],
+                          newResponseError: "",
+                        })
+                      }
+                    >
+                      <Ionicons
+                        name="close"
+                        color={"#B71914"}
+                        size={35}
+                      ></Ionicons>
+                    </Pressable>
+                    <Pressable onPress={() => this.handleAddResponse()}>
+                      <Ionicons
+                        name="checkmark-outline"
+                        color={"#B71914"}
+                        size={35}
+                      ></Ionicons>
+                    </Pressable>
+                  </View>
+                  {this.state.newImage !== "" ? (
+                    <Pressable onPress={() => this.addPicture()}>
+                      <Image
+                        style={{
+                          width: 250,
+                          height: 250,
+                          marginLeft: "auto",
+                          marginRight: "auto",
+                        }}
+                        source={{ uri: this.state.newImage }}
+                      />
+                    </Pressable>
+                  ) : (
+                    <Pressable
+                      onPress={() => this.addPicture()}
+                      style={{
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        width: 250,
+                        height: 250,
+                        backgroundColor: "#DDDDDD",
+                      }}
+                    ></Pressable>
+                  )}
+                  <TextInput
+                    placeholder="Type response action here..."
+                    style={addQuestionStyles.newResponseText}
+                    value={this.state.newAction}
+                    onChangeText={(newAction) => this.setState({ newAction })}
+                    label="newAction"
+                  ></TextInput>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      textAlign: "center",
+                      marginVertical: 10,
+                      color: "#B71914",
+                    }}
+                  >
+                    Subjects
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                    }}
+                  >
+                    <View>{checkBoxList1}</View>
+                    <View>{checkBoxList2}</View>
+                  </View>
+                  <Text style={{ color: "#B71914", textAlign: "center" }}>
+                    {this.state.newResponseError}
+                  </Text>
+                </View>
+              ) : (
+                <View style={{ marginLeft: "auto", marginRight: "auto" }}>
+                  <Pressable
+                    onPress={() => this.setState({ responseAdd: true })}
+                    style={{ paddingLeft: 10 }}
+                  >
+                    <Ionicons name="add" color={"#B71914"} size={35} />
+                  </Pressable>
+                </View>
+              )}
+              {this.state.responses.length > 1 && (
+                <View>
+                  <View
+                    style={{
+                      backgroundColor: "#DDDDDD",
+                      alignItems: "center",
+                      marginVertical: 25,
+                    }}
+                  >
+                    <Text
+                      onPress={() => {
+                        this.handleAddQuestion();
+                      }}
+                      style={{
+                        textAlign: "center",
+                        paddingVertical: 15,
+                      }}
+                    >
+                      Add new question
+                    </Text>
+                  </View>
+                  <View>
+                    <Text style={{ color: "#B71914", textAlign: "center" }}>
+                      {this.state.newQuestionError}
+                    </Text>
+                  </View>
+                </View>
+              )}
+            </View>
+          )}
+        </ScrollView>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const addQuestionStyles = StyleSheet.create({
   responseImage: {
-    width: screenWidth * (7 / 15),
-    height: screenWidth * (1 / 2),
+    width: 250,
+    height: 250,
     alignItems: "center",
     justifyContent: "flex-end",
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   action: {
     color: "white",
     backgroundColor: "rgba(0,0,0,0.5)",
     width: "100%",
+  },
+  categoryText: {
+    fontSize: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 10,
+  },
+  backButtonContainer: {
+    backgroundColor: "#DDDDDD",
+    padding: screenWidth * (1 / 50),
+    marginBottom: screenWidth * (1 / 50),
+  },
+  buttonStuff: {
+    flexDirection: "row",
+  },
+  backIcon: {
+    alignSelf: "center",
+  },
+  backText: {
+    alignSelf: "center",
+  },
+  newResponseText: {
+    fontSize: 16,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 5,
   },
 });
